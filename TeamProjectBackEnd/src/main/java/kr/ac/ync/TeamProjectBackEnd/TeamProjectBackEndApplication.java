fc8adbfd35d9d0a710ee7cbfcfe2744abbf3e936
Kim Minkyu <112453653+sulhwa001@users.noreply.github.com>
2024-09-12 14:48:36 +0900

    Add files via upload
    백엔드 개발환경 구축

---
+++ TeamProjectBackEnd/src/main/java/kr/ac/ync/TeamProjectBackEnd/TeamProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.ync.TeamProjectBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectBackEndApplication {
+	//테스트
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProjectBackEndApplication.class, args);
+	}
+
+}
