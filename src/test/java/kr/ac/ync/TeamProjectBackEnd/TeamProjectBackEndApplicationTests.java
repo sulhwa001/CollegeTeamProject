07c23410b0eb7805a25e6192f4950ae505426fcc
Kim Minkyu College <kimmin4563@naver.com>
2024-09-12 09:18:58 +0900

    팀 프로젝트 백엔드 파트 개발환경 구성

---
+++ src/test/java/kr/ac/ync/TeamProjectBackEnd/TeamProjectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.ync.TeamProjectBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
