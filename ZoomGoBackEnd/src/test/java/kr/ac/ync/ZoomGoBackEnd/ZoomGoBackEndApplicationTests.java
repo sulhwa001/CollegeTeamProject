342f2824f49173a29087b76c4c82de8fcb2593a4
Kim Minkyu College <kimmin4563@ync.ac.kr>
2024-09-25 10:22:46 +0900

    초기 개발환경 세팅

---
+++ ZoomGoBackEnd/src/test/java/kr/ac/ync/ZoomGoBackEnd/ZoomGoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.ync.ZoomGoBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomGoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
