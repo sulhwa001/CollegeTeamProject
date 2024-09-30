c7140117fa9196b32b7f14227070ee486b8fbaa9
yeonzeom <myunjum@gmail.com>
2024-09-30 14:15:57 +0900

    테스트

--- zoomgofrontend/src/App.js
+++ zoomgofrontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MainPage from './component/main.js'; // MainPage 컴포넌트 임포트
+import RegistrationPage from './component/registration.js'; // RegistrationPage 컴포넌트 임포트
+import Categorie from './component/categorie.js';
+import Header from './component/Header.js'; 
+import ProductPage from './component/detailpage.js'
+import AdminPage from './component/admin.js';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                {/* 라우트 설정 */}
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/registration" element={<RegistrationPage />} />
+                    <Route path="/categorie" element={<Categorie />} />
+                    <Route path="/detailpage" element={<ProductPage />} />
+                    <Route path="/admin" element={<AdminPage />} />
+                       
+
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
