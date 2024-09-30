28f4caa30f9e34fa52fdfe2b1e402fe3cbe78fe2
yeonzeom <myunjum@gmail.com>
2024-09-30 12:44:57 +0900

    중고나라 프론트

---
+++ zoomgofrontend/src/component/categorie.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './categorie.css'; // CSS 파일 import
+import Header from './Header'; 
+import { useNavigate } from 'react-router-dom';
+
+
+const ProductRegistration = () => {
+
+    const navigate = useNavigate();
+
+    return (
+        <div>
+         <Header />
+            <main className="main-content">
+                <section className="search-results">
+                    <h2>검색결과</h2>
+                    <nav className="breadcrumb">홈  &gt; 의류</nav>
+                    <div className="categorieproducts">
+                        {Array.from({ length: 8 }, (_, i) => (
+                            <div className="categorieproduct" key={i}>
+                                <div className="image-placeholder"></div>
+                                <p>상품{i + 1}</p>
+                                <p>10,000원</p>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="pagination">
+                        <button className="active">1</button>
+                        <button>2</button>
+                        <button>3</button>
+                        <button>4</button>
+                        <button>5</button>
+                        <button>6</button>
+                        <button className="more-btn" onClick={() => navigate('/registration')}>글쓰기</button>
+                    </div>
+                </section>
+            </main>
+        </div>
+    );
+};
+
+export default ProductRegistration;
