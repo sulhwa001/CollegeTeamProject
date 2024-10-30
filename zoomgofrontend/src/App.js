ba75b9501859bd589623e325b2c31f32edf57d90
Kim Minkyu College <kimmin4563@ync.ac.kr>
2024-10-30 16:08:41 +0900

    Test Commit

--- zoomgofrontend/src/App.js
+++ zoomgofrontend/src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import './index.css';
 import BoardList from './Board/Board_List.js';
 import InputText from './Board/Input.js';
-import { BrowserRouter, Routes } from 'react-router-dom';
-import { Route } from 'react-router-dom';
 import Detail from './Board/Detail.js';
 import Recommand from './review/Recommand_modal.js';
 import GosuReview from './review/Gosu_review.js';
